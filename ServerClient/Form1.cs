e047f07d08e1b3ff6331912d9144b18d50aec3fc
oanaunc <34318423+oanaunc@users.noreply.github.com>
2018-05-07 16:33:48 +0300

    Add files via upload

---
+++ ServerClient/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+
+namespace ServerClient
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient client;
+        public StreamReader STR;
+        public StreamWriter STW;
+        public string receive;
+        public String text_to_send;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // get my ip
+
+            IPAddress[] localIP = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach (IPAddress address in localIP)
+            {
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    textBox3.Text = address.ToString();
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) // Start Server
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, int.Parse(textBox6.Text));
+            listener.Start();
+            client = listener.AcceptTcpClient();
+            STR = new StreamReader(client.GetStream());
+            STW = new StreamWriter(client.GetStream());
+            STW.AutoFlush = true;
+
+            backgroundWorker1.RunWorkerAsync(); //Start receiving data in background
+            backgroundWorker2.WorkerSupportsCancellation = true; // Ability to cancel this thread
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e) // receive data
+        {
+            while (client.Connected)
+            {
+                try
+                {
+                    receive = STR.ReadLine();
+                    this.textBox2.Invoke(new MethodInvoker(delegate () { textBox2.AppendText("You: " + receive + "\n"); }));
+                    receive = "";
+                } catch (Exception x)
+                {
+                    MessageBox.Show(x.Message.ToString());
+                }
+
+            }
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e) // send data
+        {
+            if (client.Connected)
+            {
+                STW.WriteLine(text_to_send);
+                this.textBox2.Invoke(new MethodInvoker(delegate () { textBox2.AppendText("Me: " + text_to_send + "\n"); }));
+            }
+            else
+            {
+                MessageBox.Show("Send failed!");
+            }
+            backgroundWorker2.CancelAsync();
+        }
+
+        private void button2_Click(object sender, EventArgs e)  // Connect to server
+        {
+            client = new TcpClient();
+            IPEndPoint IP_End = new IPEndPoint(IPAddress.Parse(textBox4.Text), int.Parse(textBox5.Text));
+            try
+            {
+                client.Connect(IP_End);
+                if (client.Connected)
+                {
+                    textBox2.AppendText("Connected to Server" + "\n");
+
+                    STW = new StreamWriter(client.GetStream());
+                    STR = new StreamReader(client.GetStream());
+                    STW.AutoFlush = true;
+
+                    backgroundWorker1.RunWorkerAsync(); //Start receiving data in background
+                    backgroundWorker2.WorkerSupportsCancellation = true; // Ability to cancel this thread
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Send Button
+        {
+            if (textBox1.Text != "")
+            {
+                text_to_send = textBox1.Text;
+                backgroundWorker2.RunWorkerAsync();
+            }
+            textBox1.Text = "";
+        }
+    }
+}
